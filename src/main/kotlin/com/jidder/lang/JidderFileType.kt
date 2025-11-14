cbcf7e3bb418a742243f2b7c5b78a0652f12438a
Rikard Swahn <rikard.swahn@entur.org>
2025-11-14 14:27:35 +0100

    Revert "Cleanup"
    This reverts commit dc9850277a440ab989e6d5484fcfe834f8e166f1.

--- src/main/kotlin/com/jidder/lang/JidderFileType.kt
+++ src/main/kotlin/com/jidder/lang/JidderFileType.kt
@@ -4,9 +4,9 @@ import com.intellij.openapi.fileTypes.LanguageFileType
 import com.jidder.icons.JidderIcons
 
 object JidderFileType : LanguageFileType(JidderLanguage.INSTANCE) {
-    override fun getName(): String = "Jidder File"
+    override fun getName(): String = "Jidder"
 
-    override fun getDescription(): String = "Jidder language file"
+    override fun getDescription(): String = "Jidder language support"
 
     override fun getDefaultExtension(): String = "jdr"
 
