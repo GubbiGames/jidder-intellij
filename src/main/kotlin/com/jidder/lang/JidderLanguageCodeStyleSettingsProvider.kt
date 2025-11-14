dbd31ab75341cf8f23d1db5494d79fad63e80305
Rikard Swahn <rikard.swahn@entur.org>
2025-11-14 10:33:43 +0100

    JidderLanguageCodeStyleSettingsProvider.kt

---
+++ src/main/kotlin/com/jidder/lang/JidderLanguageCodeStyleSettingsProvider.kt
@@ -0,0 +1,29 @@
+﻿package com.jidder.lang
+
+import com.intellij.lang.Language
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings
+import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
+
+class JidderLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvider() {
+    override fun getLanguage(): Language = JidderLanguage.INSTANCE
+
+    override fun customizeDefaults(
+        commonSettings: CommonCodeStyleSettings,
+        indentOptions: CommonCodeStyleSettings.IndentOptions
+    ) {
+        indentOptions.INDENT_SIZE = 2
+        indentOptions.TAB_SIZE = 2
+        indentOptions.USE_TAB_CHARACTER = false
+        indentOptions.CONTINUATION_INDENT_SIZE = 2
+    }
+
+    /**
+     * Should not be used in current plugin, so we provide a simple sample as backup.
+     */
+    override fun getCodeSample(settingsType: SettingsType): String {
+        return """
+            // Sample Jidder code
+            Party: "Hello World"
+        """.trimIndent()
+    }
+}
